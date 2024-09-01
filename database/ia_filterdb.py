f09695dce00f8e79468ea19ddbc7388a27684bab
Superhitcinema <super4crazy1@gmail.com>
2024-09-01 12:15:20 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -33,7 +33,7 @@ class Media(Document):
         collection_name = COLLECTION_NAME
 
 
-async def save_file(media):
+    async def save_file(bot, media):
     """Save file in database"""
 
     # TODO: Find better way to get same file_id for same media to avoid duplicates
